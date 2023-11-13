caae15565797dd593f2168cf981642b341e79b84
DebjyotiKar <dev@gmail.com>
2023-11-13 11:36:14 +0530

    secound test

--- src/test/java/testcicd/testcicd/testclass.java
+++ src/test/java/testcicd/testcicd/testclass.java
@@ -1,11 +1,19 @@
 package testcicd.testcicd;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Sleeper;
 import org.testng.annotations.Test;
 
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class testclass {
@@ -22,8 +30,14 @@ public class testclass {
 		driver.manage().window().fullscreen();
 		Thread.sleep(5000);
 		System.out.println(driver.getTitle());
+		String s= driver.getTitle();
+		System.out.println(driver.getCurrentUrl());
+		Actions act=new Actions(driver);
+		WebElement ele =driver.findElement(By.xpath("//ul[@id=\"library-nav\"]/li/a[@title=\"Java Tutorial\"]"));
+		act.moveToElement(ele).build().perform();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		System.out.println(driver.getCurrentUrl());
 		driver.close();
-		
 	}
 
 }
